6b3701be0ee80804ccf05319ef62d1712c96b629
Puja Bhandari <pujabhandari01@gmail.com>
2023-09-06 15:04:13 -0500

    Update streamlit_app.py
    Changed to the chapters format

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
-streamlit.title('New API PAGE')
+streamlit.title('My Mom's New Healthy Diner')
 
 
-streamlit.header('Snowflake')
-streamlit.text('Certification 1')
-streamlit.text('Certification 2')
-streamlit.text('Certification 3')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
